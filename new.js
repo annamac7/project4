695ce772a6e92cc7f58aade9810e967bd908e6cd
annamac7 <annamac7@live.unc.edu>
2020-11-16 13:12:02 -0500

    added font awesome and additional click modal

--- new.js
+++ new.js
@@ -2,13 +2,13 @@ function resolveAfter5Sec() {
   return new Promise(resolve => {
     setTimeout(() => {
       initMap();
-		// console.log("promise")
-  }, 4000);
+      // console.log("promise")
+    }, 2000);
   });
 }
 
 async function getData() {
-console.log("get data")
+  console.log("get data")
 
   loadData()
   var result = await resolveAfter5Sec();
@@ -16,13 +16,12 @@ console.log("get data")
   console.log(data)
 }
 
-  let data = [];
+let data = [];
 
 $(document).ready(function() {
   console.log('ready! line');
   getData()
 });
-
 
 
 function loadData() {
@@ -41,328 +40,265 @@ function parseData(places) {
 
 function initMap() {
   console.log(data)
-  var styleArray = [
-    {
-        "elementType": "labels.text",
-        "stylers": [
-            {
-                "visibility": "off"
-            }
-        ]
+  var styleArray = [{
+      "elementType": "labels.text",
+      "stylers": [{
+        "visibility": "off"
+      }]
     },
     {
-        "featureType": "landscape.natural",
-        "elementType": "geometry.fill",
-        "stylers": [
-            {
-                "color": "#f5f5f2"
-            },
-            {
-                "visibility": "on"
-            }
-        ]
+      "featureType": "landscape.natural",
+      "elementType": "geometry.fill",
+      "stylers": [{
+          "color": "#f5f5f2"
+        },
+        {
+          "visibility": "on"
+        }
+      ]
     },
     {
-        "featureType": "administrative",
-        "stylers": [
-            {
-                "visibility": "off"
-            }
-        ]
+      "featureType": "administrative",
+      "stylers": [{
+        "visibility": "off"
+      }]
     },
     {
-        "featureType": "transit",
-        "stylers": [
-            {
-                "visibility": "off"
-            }
-        ]
+      "featureType": "transit",
+      "stylers": [{
+        "visibility": "off"
+      }]
     },
     {
-        "featureType": "poi.attraction",
-        "stylers": [
-            {
-                "visibility": "off"
-            }
-        ]
+      "featureType": "poi.attraction",
+      "stylers": [{
+        "visibility": "off"
+      }]
     },
     {
-        "featureType": "landscape.man_made",
-        "elementType": "geometry.fill",
-        "stylers": [
-            {
-                "color": "#ffffff"
-            },
-            {
-                "visibility": "on"
-            }
-        ]
+      "featureType": "landscape.man_made",
+      "elementType": "geometry.fill",
+      "stylers": [{
+          "color": "#ffffff"
+        },
+        {
+          "visibility": "on"
+        }
+      ]
     },
     {
-        "featureType": "poi.business",
-        "stylers": [
-            {
-                "visibility": "off"
-            }
-        ]
+      "featureType": "poi.business",
+      "stylers": [{
+        "visibility": "off"
+      }]
     },
     {
-        "featureType": "poi.medical",
-        "stylers": [
-            {
-                "visibility": "off"
-            }
-        ]
+      "featureType": "poi.medical",
+      "stylers": [{
+        "visibility": "off"
+      }]
     },
     {
-        "featureType": "poi.place_of_worship",
-        "stylers": [
-            {
-                "visibility": "off"
-            }
-        ]
+      "featureType": "poi.place_of_worship",
+      "stylers": [{
+        "visibility": "off"
+      }]
     },
     {
-        "featureType": "poi.school",
-        "stylers": [
-            {
-                "visibility": "off"
-            }
-        ]
+      "featureType": "poi.school",
+      "stylers": [{
+        "visibility": "off"
+      }]
     },
     {
-        "featureType": "poi.sports_complex",
-        "stylers": [
-            {
-                "visibility": "off"
-            }
-        ]
+      "featureType": "poi.sports_complex",
+      "stylers": [{
+        "visibility": "off"
+      }]
     },
     {
-        "featureType": "road.highway",
-        "elementType": "geometry",
-        "stylers": [
-            {
-                "color": "#ffffff"
-            },
-            {
-                "visibility": "simplified"
-            }
-        ]
+      "featureType": "road.highway",
+      "elementType": "geometry",
+      "stylers": [{
+          "color": "#ffffff"
+        },
+        {
+          "visibility": "simplified"
+        }
+      ]
     },
     {
-        "featureType": "road.arterial",
-        "stylers": [
-            {
-                "visibility": "simplified"
-            },
-            {
-                "color": "#ffffff"
-            }
-        ]
+      "featureType": "road.arterial",
+      "stylers": [{
+          "visibility": "simplified"
+        },
+        {
+          "color": "#ffffff"
+        }
+      ]
     },
     {
-        "featureType": "road.highway",
-        "elementType": "labels.icon",
-        "stylers": [
-            {
-                "color": "#ffffff"
-            },
-            {
-                "visibility": "off"
-            }
-        ]
+      "featureType": "road.highway",
+      "elementType": "labels.icon",
+      "stylers": [{
+          "color": "#ffffff"
+        },
+        {
+          "visibility": "off"
+        }
+      ]
     },
     {
-        "featureType": "road.highway",
-        "elementType": "labels.icon",
-        "stylers": [
-            {
-                "visibility": "off"
-            }
-        ]
+      "featureType": "road.highway",
+      "elementType": "labels.icon",
+      "stylers": [{
+        "visibility": "off"
+      }]
     },
     {
-        "featureType": "road.arterial",
-        "stylers": [
-            {
-                "color": "#ffffff"
-            }
-        ]
+      "featureType": "road.arterial",
+      "stylers": [{
+        "color": "#ffffff"
+      }]
     },
     {
-        "featureType": "road.local",
-        "stylers": [
-            {
-                "color": "#ffffff"
-            }
-        ]
+      "featureType": "road.local",
+      "stylers": [{
+        "color": "#ffffff"
+      }]
     },
     {
-        "featureType": "poi.park",
-        "elementType": "labels.icon",
-        "stylers": [
-            {
-                "visibility": "off"
-            }
-        ]
+      "featureType": "poi.park",
+      "elementType": "labels.icon",
+      "stylers": [{
+        "visibility": "off"
+      }]
     },
     {
-        "featureType": "poi",
-        "elementType": "labels.icon",
-        "stylers": [
-            {
-                "visibility": "off"
-            }
-        ]
+      "featureType": "poi",
+      "elementType": "labels.icon",
+      "stylers": [{
+        "visibility": "off"
+      }]
     },
     {
-        "featureType": "water",
-        "stylers": [
-            {
-                "color": "#71c8d4"
-            }
-        ]
+      "featureType": "water",
+      "stylers": [{
+        "color": "#71c8d4"
+      }]
     },
     {
-        "featureType": "landscape",
-        "stylers": [
-            {
-                "color": "#e5e8e7"
-            }
-        ]
+      "featureType": "landscape",
+      "stylers": [{
+        "color": "#e5e8e7"
+      }]
     },
     {
-        "featureType": "poi.park",
-        "stylers": [
-            {
-                "color": "#8ba129"
-            }
-        ]
+      "featureType": "poi.park",
+      "stylers": [{
+        "color": "#8ba129"
+      }]
     },
     {
-        "featureType": "road",
-        "stylers": [
-            {
-                "color": "#ffffff"
-            }
-        ]
+      "featureType": "road",
+      "stylers": [{
+        "color": "#ffffff"
+      }]
     },
     {
-        "featureType": "poi.sports_complex",
-        "elementType": "geometry",
-        "stylers": [
-            {
-                "color": "#c7c7c7"
-            },
-            {
-                "visibility": "off"
-            }
-        ]
+      "featureType": "poi.sports_complex",
+      "elementType": "geometry",
+      "stylers": [{
+          "color": "#c7c7c7"
+        },
+        {
+          "visibility": "off"
+        }
+      ]
     },
     {
-        "featureType": "water",
-        "stylers": [
-            {
-                "color": "#a0d3d3"
-            }
-        ]
+      "featureType": "water",
+      "stylers": [{
+        "color": "#a0d3d3"
+      }]
     },
     {
-        "featureType": "poi.park",
-        "stylers": [
-            {
-                "color": "#91b65d"
-            }
-        ]
+      "featureType": "poi.park",
+      "stylers": [{
+        "color": "#91b65d"
+      }]
     },
     {
-        "featureType": "poi.park",
-        "stylers": [
-            {
-                "gamma": 1.51
-            }
-        ]
+      "featureType": "poi.park",
+      "stylers": [{
+        "gamma": 1.51
+      }]
     },
     {
-        "featureType": "road.local",
-        "stylers": [
-            {
-                "visibility": "off"
-            }
-        ]
+      "featureType": "road.local",
+      "stylers": [{
+        "visibility": "off"
+      }]
     },
     {
-        "featureType": "road.local",
-        "elementType": "geometry",
-        "stylers": [
-            {
-                "visibility": "on"
-            }
-        ]
+      "featureType": "road.local",
+      "elementType": "geometry",
+      "stylers": [{
+        "visibility": "on"
+      }]
     },
     {
-        "featureType": "poi.government",
-        "elementType": "geometry",
-        "stylers": [
-            {
-                "visibility": "off"
-            }
-        ]
+      "featureType": "poi.government",
+      "elementType": "geometry",
+      "stylers": [{
+        "visibility": "off"
+      }]
     },
     {
-        "featureType": "landscape",
-        "stylers": [
-            {
-                "visibility": "off"
-            }
-        ]
+      "featureType": "landscape",
+      "stylers": [{
+        "visibility": "off"
+      }]
     },
     {
-        "featureType": "road",
-        "elementType": "labels",
-        "stylers": [
-            {
-                "visibility": "off"
-            }
-        ]
+      "featureType": "road",
+      "elementType": "labels",
+      "stylers": [{
+        "visibility": "off"
+      }]
     },
     {
-        "featureType": "road.arterial",
-        "elementType": "geometry",
-        "stylers": [
-            {
-                "visibility": "simplified"
-            }
-        ]
+      "featureType": "road.arterial",
+      "elementType": "geometry",
+      "stylers": [{
+        "visibility": "simplified"
+      }]
     },
     {
-        "featureType": "road.local",
-        "stylers": [
-            {
-                "visibility": "simplified"
-            }
-        ]
+      "featureType": "road.local",
+      "stylers": [{
+        "visibility": "simplified"
+      }]
     },
     {
-        "featureType": "road"
+      "featureType": "road"
     },
     {
-        "featureType": "road"
+      "featureType": "road"
     },
     {},
     {
-        "featureType": "road.highway"
+      "featureType": "road.highway"
     }
-]
+  ]
   // The location of florence
   const center = {
-    lat: 48.861,
-    lng:2.3376
+    lat: 48.870,
+    lng:2.3266
   };
   // The map, centered at florence
-    map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 15,
+  map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 13,
     center: center,
     mapTypeId: "roadmap",
     styles: styleArray
@@ -371,35 +307,40 @@ function initMap() {
   const markers = data.forEach(k => {
     let latLng = new google.maps.LatLng(k.lat, k.lng);
 
-    let marker =  new google.maps.Marker({
+    let marker = new google.maps.Marker({
       position: latLng,
-      label: k.name,
+      // label: k.name,
       icon: k.img,
       map: map,
-      clickable: true
-  });
-  markerArr.push(marker)
+      clickable: true,
+      id: k.name
+    });
+    markerArr.push(marker)
   });
   console.log(markerArr)
 
   new MarkerClusterer(map, markerArr, {
-  imagePath:
-    "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
-});
+    imagePath: "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
+  });
 
 
-for(let i=0; i < markerArr.length; i++){
-  map.addListener("center_changed", () => {
-    // 3 seconds after the center of the map has changed, pan back to the
-    // marker.
-    window.setTimeout(() => {
-      // map.panTo(markerArr[i].getPosition());
-    }, 3000);
-  });
-  markerArr[i].addListener("click", () => {
-    map.setZoom(16);
-    map.setCenter(markerArr[i].getPosition());
-  });
+  for (let i = 0; i < markerArr.length; i++) {
+    map.addListener("center_changed", () => {
+      // 3 seconds after the center of the map has changed, pan back to the
+      // marker.
+      window.setTimeout(() => {
+        // map.panTo(markerArr[i].getPosition());
+      }, 3000);
+    });
+
+    markerArr[i].addListener("click", () => {
+      //display a model
+      console.log(markerArr[i].id)
+      disaplyModal(markerArr[i].id)
+      map.setCenter(markerArr[i].getPosition());
+    });
+  }
+
 }
 
 
@@ -407,4 +348,36 @@ for(let i=0; i < markerArr.length; i++){
 
 
 
+function disaplyModal(place) {
+console.log(place)
+let currPlace = data.find(k => k.name == place)
+console.log(currPlace)
+  let modal = $(`<div class="card expandedCard myCard position-absolute"  style="width: 14rem" id="hasCard" style="width: 18rem;">
+  <i class="far fa-times-circle"></i>
+  <img src="${currPlace.img}" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">${currPlace.name}</h5>
+    <p class="card-text text-muted">Arrodisments: ${currPlace.arr}</p>
+
+    <p class="card-text"><small class="text-muted">Lat: ${currPlace.lat} | Long: ${currPlace.lng}</small></p>
+    <p class="card-text">${currPlace.details}</p>
+    <a href="#" class="btn myBtn" id="triggerModal">Learn More</a>
+  </div>
+</div>`);
+
+var elementExists = document.getElementById("hasCard");
+console.log(elementExists)
+if(elementExists != null) {
+elementExists.remove();
 }
+  console.log(place)
+  $('#modal').append(modal)
+}
+
+
+$(document).on('click', '#triggerModal', function(){
+  event.preventDefault();
+
+  let bigModal = $(`<div class="myModal animate__backInUp animate mx-auto my-5 shadow-lg rounded container"><i class="far fa-times-circle"></i></div>`);
+  $("#modalHere").append(bigModal);
+})
